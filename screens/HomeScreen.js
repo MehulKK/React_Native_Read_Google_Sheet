abcf40f53b796edf8187648520e65ad8ffac9c60
macv <mehul.kabaria@gmail.com>
2020-05-10 22:18:12 +0530

    Bind response with list

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import { Button, View, ActivityIndicator } from 'react-native'
+import { Button, View, ActivityIndicator, FlatList, Dimensions, TouchableOpacity, Text } from 'react-native'
+
+import FastImage from 'react-native-fast-image';
 import styles from '../styles/style'
 
 export default class HomeScreen extends React.Component {
@@ -54,12 +56,12 @@ export default class HomeScreen extends React.Component {
                             responseModel: {
                                 char_id: jsonData.feed.entry[i]['gsx$charid']['$t'],
                                 name: jsonData.feed.entry[i]['gsx$name']['$t'],
-                                birthday: jsonData.feed.entry[i]['gsx$charid']['$t'],
-                                occupation: jsonData.feed.entry[i]['gsx$charid']['$t'],
-                                img: jsonData.feed.entry[i]['gsx$charid']['$t'],
-                                status: jsonData.feed.entry[i]['gsx$charid']['$t'],
-                                nickname: jsonData.feed.entry[i]['gsx$charid']['$t'],
-                                portrayed: jsonData.feed.entry[i]['gsx$charid']['$t']
+                                birthday: jsonData.feed.entry[i]['gsx$birthday']['$t'],
+                                occupation: jsonData.feed.entry[i]['gsx$occupation']['$t'],
+                                img: jsonData.feed.entry[i]['gsx$img']['$t'],
+                                status: jsonData.feed.entry[i]['gsx$status']['$t'],
+                                nickname: jsonData.feed.entry[i]['gsx$nickname']['$t'],
+                                portrayed: jsonData.feed.entry[i]['gsx$portrayed']['$t']
                             }
                         })
                         this.state.responseList.push(this.state.responseModel)
@@ -85,12 +87,12 @@ export default class HomeScreen extends React.Component {
 
         if (this.state.isLoading) {
             return (
-              <View style={[styles.activitycontainer, styles.horizontal]}>
-                <ActivityIndicator style={{ alignContent: 'center' }} size="large" color="#334b2d" />
-              </View>
-      
+                <View style={[styles.activitycontainer, styles.horizontal]}>
+                    <ActivityIndicator style={{ alignContent: 'center' }} size="large" color="#334b2d" />
+                </View>
+
             );
-          }
+        }
         return (
             <View styles={styles.container}>
                 <Button
@@ -99,6 +101,41 @@ export default class HomeScreen extends React.Component {
                         //this.props.navigation.navigate('Detail')
                         alert(this.state.responseList.length)
                     }
+                />
+
+                <FlatList
+                    style={{ marginBottom: 50, marginTop: 10 }}
+                    data={this.state.dataSource}
+
+                    renderItem={({ item, index }) => <View style={{
+                        width: Dimensions.get('window').width / 2 - 10,
+                        height: Dimensions.get('window').width / 2 + 10, marginStart: 5, marginRight: 5, marginBottom: 5
+                    }}>
+                        <TouchableOpacity
+                            onPress={() => {
+                                //this.props.navigation.navigate('Details', {
+                                 //   data: this.state.dataSource[index]
+                                //});
+                            }}>
+                            <FastImage
+                                style={{
+                                    borderRadius: 10,
+                                    width: Dimensions.get('window').width / 2 - 10,
+                                    height: Dimensions.get('window').width / 2 - 20,
+                                    alignItems: 'center'
+                                }}
+                                source={{ uri: item.img, priority: FastImage.priority.normal }}
+                                resizeMode={FastImage.resizeMode.cover}
+                                borderRadius={10}
+
+                            />
+
+                            <Text numberOfLines={1} ellipsizeMode='tail' style={{ marginTop: 5, fontSize: 16, fontWeight: 'bold' }}> {item.nickname} </Text>
+                        </TouchableOpacity>
+                    </View>}
+
+                    numColumns={2}
+
                 />
             </View>
         );
